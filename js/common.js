e3b92d8a4a0860ff48f53f5b4cfda4b83b99ac9b
JLGR31 <j.lgonzalezromero31@gmail.com>
2024-05-02 13:28:29 +0200

    Revisar posicion header sticky, logo whatsapp añadido

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
-const btnToggle = document.querySelector("#abrir-menu");
-btnToggle.addEventListener("click", function() {
-    document.getElementById("sidebar").classList.toggle("active");
-    document.getElementById("logo").classList.toggle("animacion");
+window.addEventListener("DOMContentLoaded", function(){
+    const btnToggle = document.querySelector("#abrir-menu");
+    btnToggle.addEventListener("click", function() {
+        document.getElementById("sidebar").classList.toggle("active");
+        document.getElementById("logo").classList.toggle("animacion");
+    });
 });
+
