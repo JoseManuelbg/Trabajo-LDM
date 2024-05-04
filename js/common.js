64d49e7d2c3c43ab428af9415533f1eaaf2f5cea
JLGR31 <j.lgonzalezromero31@gmail.com>
2024-05-05 00:57:34 +0200

    Funcionalidad sidebar terminada, fotos afiliados, CSS galeria de categorias, arreglado modales productos e implementado código de Edu

--- js/common.js
+++ js/common.js
@@ -3,9 +3,17 @@ window.addEventListener("DOMContentLoaded", function(){
     btnToggle.addEventListener("click", function() {
         document.getElementById("sidebar").classList.toggle("active");
         document.getElementById("logo").classList.toggle("animacion");
-        document.getElementById("mostrar-subclases").addEventListener("click", function(){
-            document.getElementById("subclase-sidebar").classList.toggle("active-subclase");
-            console.log("Llega")
+        document.getElementById("mostrar-subclases1").addEventListener("click", function(){
+            document.getElementById("mostrar-subclases1").classList.toggle("animacion");
+            document.querySelectorAll(".subclase-sidebar1").forEach(element => {
+                element.classList.toggle("active-subclase");
+            });
+        });
+        document.getElementById("mostrar-subclases2").addEventListener("click", function(){
+            document.getElementById("mostrar-subclases2").classList.toggle("animacion");
+            document.querySelectorAll(".subclase-sidebar2").forEach(element => {
+                element.classList.toggle("active-subclase");
+            });
         });
     });
 });
