344e1473ce3982aeb9552c04a61d5fead0d59663
JLGR31 <j.lgonzalezromero31@gmail.com>
2024-05-09 20:28:55 +0200

    Creadas todas las sillas, optimizado el contador (para que funcione con todos los productos obteniendo automaticamente el precio), actualizada la carpeta de fotos, hecha funcionalidad cambio de color, y algunos detalles más

--- js/common.js
+++ js/common.js
@@ -16,5 +16,22 @@ window.addEventListener("DOMContentLoaded", function(){
             });
         });
     });
+    document.getElementById("imagen-sorteo").addEventListener("click", function () {
+        var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        swal({
+            title: "PARTICIPACIÓN EN SORTEO",
+            text: "Email: ",
+            content: "input",
+            icon: "/img/favicon.png",
+            button: "Aceptar",
+        })
+            .then((value) => {
+                if (value != null && validEmail.test(value)) {
+                    swal('Participación enviada correctamente')
+                } else {
+                    swal('Email incorrecto')
+                }
+            });
+    });
 });
 
