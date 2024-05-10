2f6f7d45bd0dabd42e7aa4ca4277ae5958c819ed
albaag03 <algueruiz@gmail.com>
2024-05-10 22:07:57 +0200

    fuentes y actualizaciones paginas

--- js/categorias.js
+++ js/categorias.js
@@ -3,12 +3,10 @@ window.addEventListener('DOMContentLoaded', function () {
     //Selecciona todos los botones de corazon (rellenos y no rellenos)
     const botonesCorazon = document.querySelectorAll('#boton-corazon');
     const botonesCorazonRelleno = document.querySelectorAll('#boton-corazon-relleno');
-    console.log('Hola');
 
     //Añade la visibilidad a cada par de botones de corazon, recorriendo el array de los botones
     botonesCorazon.forEach((boton) => {
         boton.addEventListener('click', function () {
-            console.log('Hola');
             this.style.display = 'none';
             this.parentElement.querySelector('#boton-corazon-relleno').style.display = 'block';
         });
@@ -16,7 +14,6 @@ window.addEventListener('DOMContentLoaded', function () {
 
     botonesCorazonRelleno.forEach((botonRelleno) => {
         botonRelleno.addEventListener('click', function () {
-            console.log('Hola');
             this.style.display = 'none';
             this.parentElement.querySelector('#boton-corazon').style.display = 'block';
         
