94d814d388ac6d4b7d1c36a08d33f195454293a7
Jorge González <146116659+JLGR31@users.noreply.github.com>
2024-05-11 20:38:35 +0200

    Arreglo index.js

--- js/index.js
+++ js/index.js
@@ -14,30 +14,8 @@ window.addEventListener("DOMContentLoaded", function () {
             button: "Aceptar",
         });
     });
-  document.getElementById("aceptar-cookies").addEventListener("click", function () {
-      document.getElementById("cookies").style.bottom = "-130px";
-        window.addEventListener("scroll", function () {
-            document.getElementById("cookies").style.display = "none";
-        });
-    document.getElementById("imagen-sorteo").addEventListener("click", function () {
-        var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-        swal({
-            title: "PARTICIPACIÓN EN SORTEO",
-            text: "Email: ",
-            content: "input",
-            icon: "../img/favicon.png",
-            button: "Aceptar",
-        })
-            .then((value) => {
-                if (value != null && validEmail.test(value)) {
-                    swal('Participación enviada correctamente')
-                } else {
-                    swal('Email incorrecto')
-                }
-            });
-
+    document.getElementById("aceptar-cookies").addEventListener("click", function () {
         document.getElementById("cookies").style.bottom = "-133px";
-
     });
 });
 
