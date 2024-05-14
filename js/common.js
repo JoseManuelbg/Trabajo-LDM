06d07bdff92b7bb08381e2a63e99ba80636baf28
JLGR31 <j.lgonzalezromero31@gmail.com>
2024-05-14 16:26:42 +0200

    traduccion al rumano

--- js/common.js
+++ js/common.js
@@ -38,6 +38,6 @@ window.addEventListener("DOMContentLoaded", function(){
 });
 
 function googleTranslateElementInit() {
-	new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
+	new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt,de,ro', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
 }
 
